3f95e1ca4b71986bbae4ffda98328586112fe485
Rik <h37htpc@h37htpc.h37network.net>
2014-01-05 18:33:34 +0100

    try orientation lock through javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 require(['departures/main'], function(Departure) {
     'use strict';
 
+    // We lock the screen orientation of this app to 'portrait' because
+    // we don't see any use of this app being in landscape, and it will
+    // prevent annoyances when people accidentally tilt the screen or
+    // when the orentation accelerometer of the phone is buggy.
+    //
+    // At the time of writing, this API call is experimental and only
+    // Gecko has an implementation for it under `mozLockOrientation`, so
+    // we put it inside a try.
+    try {
+        screen.mozLockOrientation('portrait');
+    } finally { }
+
     var departure = new Departure();
     departure.activate();
 
